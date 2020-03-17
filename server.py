46feb8fccb31f93f026c75ddbb35a1276edf582b
Yasmeen Atra <yas.2892@gmail.com>
2020-03-17 10:11:38 -0700

    set flask debug to false for deploying

--- server.py
+++ server.py
@@ -99,7 +99,7 @@ def tubbo_location():
 
 
 if __name__ == "__main__":
-    app.debug = True
+    app.debug = False
     app.jinja_env.auto_reload = app.debug
     connect_to_db(app)
     app.run(port=5000, host='0.0.0.0')
